b4f3907c87b4f3ccb5c0ef4b3fb598cbd848bce6
OXTS\clawford <clawford@oxts.com>
2018-01-31 16:46:37 +0000

    Combobox

--- PythonGuiProgrammingCookbook.py
+++ PythonGuiProgrammingCookbook.py
@@ -22,7 +22,7 @@ a_label.grid(column=0, row=0)
 
 # Modified Button Click Function
 def click_me():
-    action.configure(text="Hello " + name.get())
+    action.configure(text="Hello " + name.get() + ' ' + number_chosen.get())
     
 # Changing our Label
 ttk.Label(win, text="Enter a name:").grid(column=0,row=0)
@@ -34,8 +34,15 @@ name_entered.grid(column=0,row=1)
 
 # Adding a Button
 action = ttk.Button(win, text="Click Me!", command=click_me)
-action.grid(column=1, row=1)
-action.configure(state='disabled')
+action.grid(column=2, row=1)
+
+
+ttk.Label(win,text='Choose a number:').grid(column=1, row=0)
+number =tk.StringVar()
+number_chosen = ttk.Combobox(win,width = 12, textvariable = number, state = 'readonly')
+number_chosen['values'] = (1, 2, 4, 43, 100)
+number_chosen.grid(column= 1, row=1)
+number_chosen.current(0)
 
 name_entered.focus()    # Place cursor into name Entry
 
