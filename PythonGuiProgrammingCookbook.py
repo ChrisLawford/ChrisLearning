3852c70cb7e65b3cf71d459d7486024d5c79bed1
OXTS\clawford <clawford@oxts.com>
2018-01-31 15:30:45 +0000

    Added label

--- PythonGuiProgrammingCookbook.py
+++ PythonGuiProgrammingCookbook.py
@@ -9,6 +9,7 @@ Created on Wed Jan 31 14:52:24 2018
 # imports
 #======================
 import tkinter as tk
+from tkinter import ttk
 
 # Create instance
 win = tk.Tk()   
@@ -16,6 +17,8 @@ win = tk.Tk()
 # Add a title       
 win.title("Python GUI")  
 
+# Adding a Label
+ttk.Label(win, text="A Label").grid(column=0, row=0) 
 
 #======================
 # Start GUI
