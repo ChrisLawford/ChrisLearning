ba4e86ef2727af85607eb3ab93617fd5d5ffe4a8
OXTS\clawford <clawford@oxts.com>
2018-01-31 16:54:51 +0000

    Checkbuttons

--- PythonGuiProgrammingCookbook.py
+++ PythonGuiProgrammingCookbook.py
@@ -36,7 +36,7 @@ name_entered.grid(column=0,row=1)
 action = ttk.Button(win, text="Click Me!", command=click_me)
 action.grid(column=2, row=1)
 
-
+# Creating three checkbuttons
 ttk.Label(win,text='Choose a number:').grid(column=1, row=0)
 number =tk.StringVar()
 number_chosen = ttk.Combobox(win,width = 12, textvariable = number, state = 'readonly')
@@ -44,8 +44,22 @@ number_chosen['values'] = (1, 2, 4, 43, 100)
 number_chosen.grid(column= 1, row=1)
 number_chosen.current(0)
 
-name_entered.focus()    # Place cursor into name Entry
+chVarDis = tk.IntVar()
+check1 = tk.Checkbutton(win, text ="Disabled", variable = chVarDis, state= 'disabled')
+check1.select()
+check1.grid(column=0, row=4, sticky=tk.W)
 
+chVarUn = tk.IntVar()
+check2 = tk.Checkbutton(win, text ="Unchecked", variable = chVarUn)
+check2.deselect()
+check2.grid(column=1, row=4, sticky=tk.W)
+
+chVarEn = tk.IntVar()
+check3 = tk.Checkbutton(win, text ="Enabled", variable = chVarEn)
+check3.select()
+check3.grid(column=2, row=4, sticky=tk.W)
+
+name_entered.focus()    # Place cursor into name Entry
 #======================
 # Start GUI
 #======================
