206ec0fcf5f8620bc3a5dae639e592644623c8e4
OXTS\clawford <clawford@oxts.com>
2018-01-31 15:01:00 +0000

    First_GUI

---
+++ PythonGuiProgrammingCookbook.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 31 14:52:24 2018
+
+@author: clawford
+"""
+
+#======================
+# imports
+#======================
+import tkinter as tk
+
+# Create instance
+win = tk.Tk()   
+
+# Add a title       
+win.title("Python GUI")  
+
+
+#======================
+# Start GUI
+#======================
+win.mainloop()
